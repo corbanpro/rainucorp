c816ba7359ab87c92d0cbc1b3cf86559b012d2b5
Corban Procuniar <103233091+corbanpro@users.noreply.github.com>
2024-02-25 17:06:17 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.page}>
+      <div style={styles.container}>
+        <div className="display-1">Welcome to Rainucorp!!</div>
+        <p className="lead fs-3">More coming soon...</p>
+      </div>
     </div>
   );
 }
+
+const styles = {
+  page: {
+    height: "100vh",
+    backgroundColor: "#282c34",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    textAlign: "center",
+  },
+} as const;
 
 export default App;
